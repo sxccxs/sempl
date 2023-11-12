73912e6e8ded7189e7acce1083a0a3c875f7e9c9
Hryhorii Biloshenko <biloshry@cvut.cz>
2023-11-12 21:26:51 +0100

    fix: remove constructor from lexer interface

--- lexer/interfaces.py
+++ lexer/interfaces.py
@@ -1,12 +1,8 @@
-# pylint: disable=C0116,R0903
-from typing import Protocol, TextIO
+from typing import Protocol
 
 from lexer.tokens import Token
 
 
 class ILexer(Protocol):
-    def __init__(self, input_io: TextIO) -> None:
-        ...
-
-    def next_token(self) -> Token:
+    def next_token(self) -> Token:  # pylint: disable=C0116
         ...
