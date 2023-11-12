2cee491c1ec5696b4b76f6021042e4ba1901e2f8
Hryhorii Biloshenko <biloshry@cvut.cz>
2023-11-12 16:58:33 +0100

    test: test lexer with one literal per test.

--- tests/tg_lexer_tests.py
+++ tests/tg_lexer_tests.py
@@ -5,6 +5,43 @@ import pytest
 from lexer.lexer import Lexer
 from lexer.tokens import Token, TokenType
 
+literal_token_map: dict[str, Token] = {
+    tt.value: Token(tt, tt.value)
+    for tt in [
+        TokenType.LPAREN,
+        TokenType.RPAREN,
+        TokenType.COLON,
+        TokenType.ARROW,
+        TokenType.ASSIGN,
+        TokenType.PLUS,
+        TokenType.MINUS,
+        TokenType.ASTERIX,
+        TokenType.SLASH,
+        TokenType.APOSTROPHE,
+        TokenType.EQ,
+        TokenType.NOT_EQ,
+        TokenType.GT,
+        TokenType.LT,
+        TokenType.GTEQ,
+        TokenType.LTEQ,
+    ]
+} | {
+    "\n": Token(TokenType.ENDL, "\n"),
+    "\0": Token(TokenType.EOF, "\0"),
+    " ": Token(TokenType.SPACE, " "),
+    "`": Token(TokenType.ILLEGAL, "`"),
+    "name": Token(TokenType.IDENT, "name"),
+    "10": Token(TokenType.INT, "10"),
+    "1.5": Token(TokenType.FLOAT, "1.5"),
+    "if": Token(TokenType.IF, "if"),
+    "else": Token(TokenType.ELSE, "else"),
+    "return": Token(TokenType.RETURN, "return"),
+    "mut": Token(TokenType.MUT, "mut"),
+    "let": Token(TokenType.LET, "let"),
+    "True": Token(TokenType.TRUE, "True"),
+    "False": Token(TokenType.FALSE, "False"),
+}
+
 input_and_expected: dict[str, list[Token]] = {
     "+-/*=\n)(: '><\n": [
         Token(TokenType.PLUS, "+"),
@@ -59,20 +96,36 @@ input_and_expected: dict[str, list[Token]] = {
 
 
 class TestLexer:
-    @pytest.mark.parametrize("input_,expected", list(input_and_expected.items()))
-    def test_lexer(self, input_: str, expected: list[Token]) -> None:
+    @pytest.mark.parametrize(("input_", "expected"), list(input_and_expected.items()))
+    def test_multiple_literals(self, input_: str, expected: list[Token]) -> None:
         """
-        Tests lexer reading.
+        Tests lexer reading multiple literals correctly.
 
-        Arrange: Put input string to a StringIO.
+        Arrange: Put string of literals to a StringIO.
         Arrange: Create a lexer with the StringIO object.
 
         Cycle: For each expected token
             Act: Read a token from lexer.
-            Assert: Read token equals to expected token.
+            Assert: Token from lexer equals to expected token.
         """
         sio = StringIO(input_)
         lexer = Lexer(sio)
         for i, expected_token in enumerate(expected):
             token = lexer.next_token()
             assert token == expected_token, f"Token #{i} is wrong. {token} != {expected_token}"
+
+    @pytest.mark.parametrize(("literal", "expected_token"), literal_token_map.items())
+    def test_one_token(self, literal: str, expected_token: Token) -> None:
+        """
+        Tests lexer reading one literal correctly.
+
+        Arrange: Put literal to a StringIO.
+        Arrange: Create a lexer with the StringIO object.
+
+        Act: Read a token from lexer.
+        Assert: Token from lexer equals to expected token.
+        """
+        sio = StringIO(literal)
+        lexer = Lexer(sio)
+        token = lexer.next_token()
+        assert token == expected_token, f"For {literal=} expected {expected_token}, got {token}."
