4b42889911569ba572722a3bb969d5cf6ff26a99
Hryhorii Biloshenko <biloshry@cvut.cz>
2023-11-26 20:11:08 +0100

    feat: add base parser

---
+++ src/parser/parser.py
@@ -0,0 +1,21 @@
+from result import Result
+
+from src.ast_ import ast_nodes
+from src.lexer.interfaces import ILexer
+from src.parser.errors import ParsingError
+
+
+class Parser:
+    def __init__(self, lexer: ILexer) -> None:
+        self.lexer = lexer
+        self.current_token = self.lexer.next_token()
+        self.peek_token = self.lexer.next_token()
+
+    def next_token(self) -> None:
+        """Moves parser to the next token."""
+        self.current_token = self.peek_token
+        self.peek_token = self.lexer.next_token()
+
+    def parse_program(self) -> Result[ast_nodes.Program, ParsingError]:
+        """Parses the whole program."""
+        ...
