b8f78b9abcf452c8c3e323dd42773f9e5a2c88b0
Hryhorii Biloshenko <biloshry@cvut.cz>
2023-12-02 23:18:35 +0100

    test: update lexer tests correspodingly

--- tests/tg_lexer_tests.py
+++ tests/tg_lexer_tests.py
@@ -28,7 +28,6 @@ LITERAL_TOKEN_MAP: dict[str, Token] = {
 } | {
     "\n": Token(TokenType.ENDL, "\n"),
     "\0": Token(TokenType.EOF, "\0"),
-    " ": Token(TokenType.SPACE, " "),
     "`": Token(TokenType.ILLEGAL, "`"),
     "name": Token(TokenType.IDENT, "name"),
     "10": Token(TokenType.INT, "10"),
@@ -53,42 +52,27 @@ INPUT_AND_EXPECTED: dict[str, list[Token]] = {
         Token(TokenType.RPAREN, ")"),
         Token(TokenType.LPAREN, "("),
         Token(TokenType.COLON, ":"),
-        Token(TokenType.SPACE, " "),
         Token(TokenType.APOSTROPHE, "'"),
         Token(TokenType.GT, ">"),
         Token(TokenType.LT, "<"),
         Token(TokenType.ENDL, "\n"),
-        Token(TokenType.ILLEGAL, "\t"),
         Token(TokenType.EOF, "\0"),
     ],
     "5 sqrt == != <= >= -> 10000. \n if num == 2: \n\n": [
         Token(TokenType.INT, "5"),
-        Token(TokenType.SPACE, " "),
         Token(TokenType.IDENT, "sqrt"),
-        Token(TokenType.SPACE, " "),
         Token(TokenType.EQ, "=="),
-        Token(TokenType.SPACE, " "),
         Token(TokenType.NOT_EQ, "!="),
-        Token(TokenType.SPACE, " "),
         Token(TokenType.LTEQ, "<="),
-        Token(TokenType.SPACE, " "),
         Token(TokenType.GTEQ, ">="),
-        Token(TokenType.SPACE, " "),
         Token(TokenType.ARROW, "->"),
-        Token(TokenType.SPACE, " "),
         Token(TokenType.FLOAT, "10000."),
-        Token(TokenType.SPACE, " "),
         Token(TokenType.ENDL, "\n"),
-        Token(TokenType.SPACE, " "),
         Token(TokenType.IF, "if"),
-        Token(TokenType.SPACE, " "),
         Token(TokenType.IDENT, "num"),
-        Token(TokenType.SPACE, " "),
         Token(TokenType.EQ, "=="),
-        Token(TokenType.SPACE, " "),
         Token(TokenType.INT, "2"),
         Token(TokenType.COLON, ":"),
-        Token(TokenType.SPACE, " "),
         Token(TokenType.ENDL, "\n"),
         Token(TokenType.ENDL, "\n"),
         Token(TokenType.EOF, "\0"),
@@ -113,7 +97,9 @@ class TestLexer:
         lexer = Lexer(sio)
         for i, expected_token in enumerate(expected):
             token = lexer.next_token()
-            assert token == expected_token, f"Token #{i} is wrong. {token} != {expected_token}"
+            assert (
+                token == expected_token
+            ), f"Token #{i} is wrong. {token} != {expected_token}"
 
     @pytest.mark.parametrize(("literal", "expected_token"), LITERAL_TOKEN_MAP.items())
     def test_one_token(self, literal: str, expected_token: Token) -> None:
@@ -129,4 +115,6 @@ class TestLexer:
         sio = StringIO(literal)
         lexer = Lexer(sio)
         token = lexer.next_token()
-        assert token == expected_token, f"For {literal=} expected {expected_token}, got {token}."
+        assert (
+            token == expected_token
+        ), f"For {literal=} expected {expected_token}, got {token}."
