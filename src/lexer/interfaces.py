7438e3b32351864343addef3179473eb7e4a8496
Hryhorii Biloshenko <biloshry@cvut.cz>
2023-12-02 22:11:23 +0100

    docs: add docstring to lexer interface

--- src/lexer/interfaces.py
+++ src/lexer/interfaces.py
@@ -1,8 +1,10 @@
+# pylint: disable=W2301
 from typing import Protocol
 
 from src.lexer.tokens import Token
 
 
 class ILexer(Protocol):
-    def next_token(self) -> Token:  # pylint: disable=C0116
+    def next_token(self) -> Token:
+        """Reads a token."""
         ...
