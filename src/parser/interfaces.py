cf4fc5d70f4cbcfe7c5c1d9f4963553182797bde
Hryhorii Biloshenko <biloshry@cvut.cz>
2023-12-02 22:32:51 +0100

    fix: move implementation of elementary methods to parser protocol and remove them from parser. fix: make parser fileds private and use properties

--- src/parser/interfaces.py
+++ src/parser/interfaces.py
@@ -21,14 +21,17 @@ class IParser(Protocol):
 
     def cur_token_is(self, token_t: TokenType) -> bool:
         """True if parser's current token equals to token_t else False."""
-        ...
+        return self.current_token.type == token_t
 
     def peek_token_is(self, token_t: TokenType) -> bool:
         """True if parser's peek token equals to token_t else False."""
-        ...
+        return self.peek_token.type == token_t
 
     def move_to_next_if_peek_is_expected(self, expected_t: TokenType) -> bool:
         """If parser's peek token equals to token_t, moves to next token and returns True.
         Otherwise just returns False.
         """
-        ...
+        if not self.peek_token_is(expected_t):
+            return False
+        self.next_token()
+        return True
--- src/parser/parser.py
+++ src/parser/parser.py
@@ -2,41 +2,33 @@ from result import Result
 
 from src.ast_ import ast_nodes
 from src.lexer.interfaces import ILexer
-from src.lexer.tokens import TokenType
+from src.lexer.tokens import Token
 from src.parser.errors import ParsingError
+from src.parser.interfaces import IParser
 
 
-class Parser:
+class Parser(IParser):
     def __init__(self, lexer: ILexer) -> None:
         """
         Args:
             lexer (ILexer): Lexer object providing tokens to the parser.
         """
         self.lexer = lexer
-        self.current_token = self.lexer.next_token()
-        self.peek_token = self.lexer.next_token()
+        self._current_token = self.lexer.next_token()
+        self._peek_token = self.lexer.next_token()
+
+    @property
+    def current_token(self) -> Token:
+        return self._current_token
+
+    @property
+    def peek_token(self) -> Token:
+        return self._peek_token
 
     def next_token(self) -> None:
         """Moves parser to the next token."""
-        self.current_token = self.peek_token
-        self.peek_token = self.lexer.next_token()
-
-    def cur_token_is(self, token_t: TokenType) -> bool:
-        """True if parser's current token equals to token_t else False."""
-        return self.current_token.type == token_t
-
-    def peek_token_is(self, token_t: TokenType) -> bool:
-        """True if parser's peek token equals to token_t else False."""
-        return self.peek_token.type == token_t
-
-    def move_to_next_if_peek_is_expected(self, expected_t: TokenType) -> bool:
-        """If parser's peek token equals to token_t, moves to next token and returns True.
-        Otherwise just returns False.
-        """
-        if not self.peek_token_is(expected_t):
-            return False
-        self.next_token()
-        return True
+        self._current_token = self._peek_token
+        self._peek_token = self.lexer.next_token()
 
     def parse_program(self) -> Result[ast_nodes.Program, ParsingError]:
         """Parses the whole program."""
