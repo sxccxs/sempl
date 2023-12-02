bf83f02e7bbe4cb741387174ee1b5adfe9fb089a
Hryhorii Biloshenko <biloshry@cvut.cz>
2023-12-02 22:15:05 +0100

    feat: make parser implement IParser

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -2,6 +2,7 @@ from result import Result
 
 from src.ast_ import ast_nodes
 from src.lexer.interfaces import ILexer
+from src.lexer.tokens import TokenType
 from src.parser.errors import ParsingError
 
 
@@ -19,6 +20,23 @@ class Parser:
         """Moves parser to the next token."""
         self.current_token = self.peek_token
         self.peek_token = self.lexer.next_token()
+
+    def cur_token_is(self, token_t: TokenType) -> bool:
+        """True if parser's current token equals to token_t else False."""
+        return self.current_token.type == token_t
+
+    def peek_token_is(self, token_t: TokenType) -> bool:
+        """True if parser's peek token equals to token_t else False."""
+        return self.peek_token.type == token_t
+
+    def move_to_next_if_peek_is_expected(self, expected_t: TokenType) -> bool:
+        """If parser's peek token equals to token_t, moves to next token and returns True.
+        Otherwise just returns False.
+        """
+        if not self.peek_token_is(expected_t):
+            return False
+        self.next_token()
+        return True
 
     def parse_program(self) -> Result[ast_nodes.Program, ParsingError]:
         """Parses the whole program."""
